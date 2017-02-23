60daefe6d67433f4c33dbfdad837789f709b6956
Manmohan <manmohan.kumar@techmahindra.com>
2017-02-23 22:25:06 +0530

    Added Score service client

---
+++ src/TeleSign.Services.Score/RawScoreService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace TeleSign.Services.Score
+{
+    public class RawScoreService: TeleSignService
+    {
+        private const String SCORE_RESOURCE = "/v1/score/{0}";	
+        public RawScoreService(TeleSignServiceConfiguration configuration) : base(configuration, null) { }
+
+        public TSResponse ScoreRaw(String phoneNumber, String accountLifecycleEvent, Dictionary<String, String> scoreParams = null) {
+            phoneNumber = this.CleanupPhoneNumber(phoneNumber);
+
+            if (null == scoreParams)
+                scoreParams = new Dictionary<string, string>();
+            scoreParams.Add("phone_number", phoneNumber);
+            scoreParams.Add("account_lifecycle_event", accountLifecycleEvent);
+
+            string resourceName = string.Format(SCORE_RESOURCE, phoneNumber);
+
+            WebRequest request = this.ConstructWebRequest(resourceName, "POST", scoreParams, AuthenticationMethod.HmacSha256);
+
+            return this.WebRequester.ReadTeleSignResponse(request);
+        }
+    }
+}
---
+++ src/TeleSign.Services.Score/ScoreClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TeleSign.Services.Score
+{
+    /// <summary>
+    /// Score provides risk information about a specified phone number.
+    /// </summary>
+    public class ScoreClient:RawScoreService
+    {
+        public ScoreClient(TeleSignServiceConfiguration configuration): base(configuration) { }
+
+        /// <summary>
+        /// Score is an API that delivers reputation scoring based on phone number intelligence, traffic patterns, machine learning, and a global data consortium. See https://developer.telesign.com/docs/rest_api-phoneid-score for detailed API documentation.         
+        /// </summary>
+        /// <param name="phoneNumber"></param>
+        /// <param name="accountLifecycleEvent"></param>        
+        /// <param name="scoreParams"></param>
+        /// <returns></returns>
+        public TSResponse Score(String phoneNumber, String accountLifecycleEvent, Dictionary<String, String> scoreParams = null)
+        {
+            phoneNumber = this.CleanupPhoneNumber(phoneNumber);
+            CheckArgument.NotNullOrEmpty(accountLifecycleEvent, "account_lifecycle_event");
+
+            TSResponse response = new TSResponse();
+            try
+            {
+                response = this.ScoreRaw(phoneNumber, accountLifecycleEvent, scoreParams);
+            }
+            catch (Exception e)
+            {
+                throw new ResponseParseException(
+                            "Error parsing Score response",
+                            response.ToString(),
+                            e);
+            }
+            return response;
+        }
+    }
+}
