ecda33f0570a2447aa9a02c8f877d114851462fc
Manmohan <manmohan.kumar@techmahindra.com>
2017-02-23 22:24:34 +0530

    Added Messaging client

---
+++ src/TeleSign.Services.Messaging/MessagingClient.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TeleSign.Services.Messaging
+{
+    public class MessagingClient:RawMessagingService
+    {
+        public MessagingClient(TeleSignServiceConfiguration configuration): base(configuration) { }
+
+        /// <summary>
+        /// Send a message to the target phone_number.See <a href ="https://developer.telesign.com/v2.0/docs/messaging-api">for detailed API documentation</a>.         
+        /// </summary>
+        /// <param name="phoneNumber"></param>
+        /// <param name="message"></param>
+        /// <param name="messageType"></param>
+        /// <param name="messageParams"></param>
+        /// <returns></returns>
+        public TSResponse Message(String phoneNumber, String message,
+                String messageType, Dictionary<String, String> messageParams = null)
+        {
+            phoneNumber = this.CleanupPhoneNumber(phoneNumber);
+            TSResponse response = new TSResponse();
+            try
+            {
+                response = this.MessageRaw(phoneNumber, message, messageType, messageParams);
+            }
+            catch (Exception e)
+            {
+                throw new ResponseParseException(
+                            "Error parsing Message response",
+                            response.ToString(),
+                            e);
+            }
+
+            return response;
+        }
+   
+        /// <summary>
+        /// Retrieves the current status of the message. See <a href="https://developer.telesign.com/v2.0/docs/messaging-api"> for detailed API documentation</a>.
+        /// </summary>
+        /// <param name="referenceId"></param>
+        /// <param name="statusParams"></param>
+        /// <returns></returns>
+        public TSResponse Status(String referenceId,
+                Dictionary<String, String> statusParams=null)
+        {
+
+            TSResponse response = new TSResponse();
+            try
+            {
+                response = this.StatusRaw(referenceId, statusParams);
+                return response;
+            }
+            catch (Exception e)
+            {
+
+                throw new ResponseParseException(
+                            "Error parsing Message response",
+                            response.ToString(),
+                            e);
+            }            
+        }
+    }
+}
---
+++ src/TeleSign.Services.Messaging/RawMessagingService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+namespace TeleSign.Services.Messaging
+{
+    public class RawMessagingService : TeleSignService
+    {
+        private const String MESSAGING_RESOURCE = "/v1/messaging";
+        private const String MESSAGING_STATUS_RESOURCE = "/v1/messaging/{0}";
+
+        public RawMessagingService(TeleSignServiceConfiguration configuration) :base(configuration, null) { }
+
+        public TSResponse MessageRaw(string phoneNumber, string message, string messageType, Dictionary<string, string> messageParams = null) {
+            phoneNumber = this.CleanupPhoneNumber(phoneNumber);
+
+            if (null == messageParams)
+                messageParams = new Dictionary<string, string>();
+
+            messageParams.Add("phone_number", phoneNumber);
+            messageParams.Add("message", message);
+            messageParams.Add("message_type", messageType);
+
+            WebRequest request = this.ConstructWebRequest(MESSAGING_RESOURCE, "POST", messageParams, AuthenticationMethod.HmacSha256);
+
+            return this.WebRequester.ReadTeleSignResponse(request);
+        }
+
+        public TSResponse StatusRaw(string referenceId, Dictionary<String, String> statusParams = null) {
+            CheckArgument.NotNullOrEmpty(referenceId, "reference_id");
+            if (null == statusParams)
+                statusParams = new Dictionary<string, string>();
+            statusParams.Add("reference_id", referenceId);
+
+            string resourceName = string.Format(MESSAGING_STATUS_RESOURCE, referenceId);
+
+            WebRequest request = this.ConstructWebRequest(resourceName, "GET", statusParams,AuthenticationMethod.HmacSha256);
+
+            return this.WebRequester.ReadTeleSignResponse(request);
+
+        }
+    }
+}
