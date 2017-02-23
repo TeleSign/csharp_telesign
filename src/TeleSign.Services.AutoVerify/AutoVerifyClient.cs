e1e13b636a2af76285126e1b92b2a7a1e6f6ea84
Manmohan <manmohan.kumar@techmahindra.com>
2017-02-23 22:23:40 +0530

    Added AutoVerify client

---
+++ src/TeleSign.Services.AutoVerify/AutoVerifyClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeleSign.Services.AutoVerify
+{
+    public class AutoVerifyClient : RawAutoVerifyService
+    {
+        public AutoVerifyClient(TeleSignServiceConfiguration configuration) : base(configuration)
+        {
+        }
+
+        public TSResponse Status(string externalId, Dictionary<String, String> statusParams = null)
+        {
+            CheckArgument.NotNullOrEmpty(externalId, "external_id");
+
+            TSResponse response = new TSResponse();
+            try
+            {
+                response = this.StatusRaw(externalId, statusParams);
+            }
+            catch (Exception e)
+            {
+                throw new ResponseParseException(
+                            "Error parsing Message response",
+                            response.ToString(),
+                            e);
+            }
+            return response;
+        }
+    }
+}
