72cbc3faf9bac9791776077ac6a763403bf8fb05
Jarrad Lee <jarrad@telesign.com>
2017-05-22 15:07:21 -0700

    Renamed folders to match project structure. Removed redundant libs/docs.

---
+++ src/TeleSign/PhoneIdClient.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace Telesign
+{
+    /// <summary>
+    /// Score provides risk information about a specified phone number.
+    /// </summary>
+    public class PhoneIdClient : RestClient
+    {
+        private const string SCORE_RESOURCE = "/v1/phoneid/{0}";
+
+        public PhoneIdClient(string customerId,
+                                string apiKey)
+            : base(customerId,
+                   apiKey)
+        { }
+
+        public PhoneIdClient(string customerId,
+                                string apiKey,
+                                string restEndPoint)
+            : base(customerId,
+                   apiKey,
+                   restEndPoint)
+        { }
+
+        public PhoneIdClient(string customerId,
+                                string apiKey,
+                                string restEndPoint,
+                                int timeout,
+                                WebProxy proxy,
+                                string proxyUsername,
+                                string proxyPassword)
+            : base(customerId,
+                   apiKey,
+                   restEndPoint,
+                   timeout,
+                   proxy,
+                   proxyUsername,
+                   proxyPassword)
+        { }
+
+        /// <summary>
+        /// Score is an API that delivers reputation scoring based on phone number intelligence, 
+        /// traffic patterns, machine learning, and a global data consortium. 
+        /// See https://developer.telesign.com/docs/rest_api-phoneid-score for detailed API documentation.         
+        /// </summary>
+        /// <param name="phoneNumber"></param>
+        /// <param name="accountLifecycleEvent"></param>        
+        /// <param name="scoreParams"></param>
+        /// <returns></returns>
+        public TelesignResponse PhoneId(string phoneNumber, Dictionary<string, string> phoneIdParams = null) {            
+
+            
+            if (null == phoneIdParams)
+                phoneIdParams = new Dictionary<string, string>();
+
+            string resource = string.Format(SCORE_RESOURCE, phoneNumber);            
+
+            return Post(resource, phoneIdParams);
+        }
+    }
+}
---
+++ src/TeleSign/VoiceClient.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace Telesign
+{
+    public class VoiceClient : RestClient
+    {
+        private const string VOICE_RESOURCE = "/v1/voice";
+        private const string VOICE_STATUS_RESOURCE = "/v1/voice/{0}";
+
+        public VoiceClient(string customerId,
+                                string apiKey)
+            : base(customerId,
+                   apiKey)
+        { }
+
+        public VoiceClient(string customerId,
+                                string apiKey,
+                                string restEndPoint)
+            : base(customerId,
+                   apiKey,
+                   restEndPoint)
+        { }
+
+        public VoiceClient(string customerId,
+                                string apiKey,
+                                string restEndPoint,
+                                int timeout,
+                                WebProxy proxy,
+                                string proxyUsername,
+                                string proxyPassword)
+            : base(customerId,
+                   apiKey,
+                   restEndPoint,
+                   timeout,
+                   proxy,
+                   proxyUsername,
+                   proxyPassword)
+        { }
+
+        /// <summary>
+        /// Send a voice call to the target phone_number. See https://developer.telesign.com/docs/voice-api for detailed API documentation.
+        /// </summary>
+        /// <param name="phoneNumber"></param>
+        /// <param name="message"></param>
+        /// <param name="messageType"></param>
+        /// <param name="callParams"></param>
+        /// <returns></returns>
+        public TelesignResponse Call(string phoneNumber, string message, string messageType, Dictionary<string, string> callParams = null)
+        {
+            if (null == callParams)
+                callParams = new Dictionary<string, string>();
+
+            callParams.Add("phone_number", phoneNumber);
+            callParams.Add("message", message);
+            callParams.Add("message_type", messageType);
+
+            return Post(VOICE_RESOURCE,callParams);
+        }
+
+        /// <summary>
+        /// Retrieves the current status of the voice call. See https://developer.telesign.com/docs/voice-api for detailed API documentation.
+        /// </summary>
+        /// <param name="referenceId"></param>
+        /// <param name="statusParams"></param>
+        /// <returns></returns>
+        public TelesignResponse Status(string referenceId, Dictionary<string, string> statusParams = null)
+        {
+            if (null == statusParams)
+                statusParams = new Dictionary<string, string>();
+            statusParams.Add("reference_id", referenceId);
+
+            string resource = string.Format(VOICE_STATUS_RESOURCE, referenceId);            
+
+            return Get(resource, statusParams);
+        }
+    }
+}
