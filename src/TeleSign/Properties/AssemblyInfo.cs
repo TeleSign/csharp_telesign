ec035b4dd4fba19069906085fb4048f51e765c7f
Jarrad Lee <jarrad@telesign.com>
2017-05-23 12:50:17 -0700

    Moved nuspec into Telesign project.

--- src/TeleSign/Properties/AssemblyInfo.cs
+++ src/TeleSign/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TeleSign")]
+[assembly: AssemblyTitle("Telesign")]
 [assembly: AssemblyDescription("The TeleSign C# SDK lets you easily integrate with our REST API.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Telesign")]
-[assembly: AssemblyProduct("TeleSign")]
+[assembly: AssemblyProduct("Telesign")]
 [assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
